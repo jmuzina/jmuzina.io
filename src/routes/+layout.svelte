5c0a6b26b3cbdd4a14e6d58b39da5a0f0f200dde
Julie Muzina <jmuzina@jmuzina.io>
2026-09-13 02:06:52 -0400

    a lot prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import '../app.css';
+	import { Navigation } from '$lib/components';
 
 	let { children } = $props();
+	const year = $derived(() => new Date().getFullYear());
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link href={favicon} rel="icon" />
+</svelte:head>
 
-{@render children()}
+<a
+	class="sr-only focus-within:not-sr-only focus-within:absolute inset-s-2 inset-bs-4 z-10 bg-tertiary-800"
+	href="#main-content"
+>
+	Skip to main content
+</a>
+
+<div class="grid grid-rows-[auto_1fr_auto]">
+	<header>
+		<Navigation />
+	</header>
+	<div class="container mx-auto">
+		<main class="p-4 outline-none" id="main-content" tabindex="-1">
+			{@render children()}
+		</main>
+	</div>
+	<footer class="border-t-1 border-t-primary-900-300 pbs-2">
+		<div class="flex justify-center">
+			<span class="text-tertiary-950-50">© Julie Mužina {year()}</span>
+		</div>
+	</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Envelope, Github, Linkedin } from '@boxicons/svelte';
+	import { HouseHeart, MapPin } from '@lucide/svelte';
+</script>
+
+<div class="flex flex-col gap-8">
+	<section>
+		<h2 class="text-2xl">Hi, I'm Julie!</h2>
+		<p>I build things. Step on in.</p>
+
+		<div class="flex flex-col sm:flex-row gap-x-4 gap-y-0.5 text-sm text-primary-950-50">
+			<div class="flex items-center gap-1">
+				<MapPin class="text-current" size="16" />
+				New York, New York
+			</div>
+			<div class="flex items-center gap-1">
+				<HouseHeart size="16" />
+				Cleveland, Ohio
+			</div>
+		</div>
+	</section>
+	<section>
+		<h2 class="text-xl">Under construction</h2>
+		<p>
+			I'm current rebuilding my portfolio from scratch! I've learned a lot since I built the old
+			version and want to build something fresher.
+		</p>
+
+		<p>For now, here's some other places to find me.</p>
+		<ul class="container mx-auto grid sm:grid-flow-col sm:auto-cols-min gap-x-2 gap-y-2 text-sm">
+			<!--			todo update resume is tragically out of date -->
+			<!--			<li>-->
+			<!--				<a href="#">-->
+			<!--					<div class="card preset-filled-surface-100-900 p-4 flex items-center gap-4"-->
+			<!--					>-->
+			<!--						<FileUser />-->
+			<!--						CV-->
+			<!--					</div>-->
+			<!--				</a>-->
+			<!--			</li>-->
+			<li>
+				<a
+					class="card bg-primary-50 text-primary-contrast-50 p-2 flex items-center gap-2"
+					href="https://github.com/jmuzina"
+				>
+					<Github />
+					GitHub
+				</a>
+			</li>
+			<li>
+				<a
+					class="card bg-primary-50 text-primary-contrast-50 p-2 flex items-center gap-2"
+					href="https://linkedin.com/in/jmuzina"
+				>
+					<Linkedin />
+					LinkedIn
+				</a>
+			</li>
+			<li>
+				<a
+					class="card bg-primary-50 text-primary-contrast-50 p-2 flex items-center gap-2"
+					href="mailto:jmuzina@pm.me"
+				>
+					<Envelope />
+					Email
+				</a>
+			</li>
+		</ul>
+	</section>
+</div>
