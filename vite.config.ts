f525605ea290cae059577ae3fb379af6f37be1ae
Julie Muzina <jmuzina@jmuzina.io>
2026-09-13 23:45:50 -0400

    wip blog

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+	extensions: ['.svelte', '.md', '.svx'],
+	preprocess: [
+		vitePreprocess(),
+		// 3. Add mdsvex to the preprocessor pipeline
+		mdsvex({
+			extensions: ['.md', '.svx']
+		})
+	],
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
 });
